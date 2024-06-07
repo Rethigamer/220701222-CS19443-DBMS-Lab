e0e8a81027083e8700968d8ef30d1b498b2e1bde
Rethigamer <rethinaath@gmail.com>
2024-06-07 14:58:48 +0530

    modified connection details

--- mini_project/connection.py
+++ mini_project/connection.py
@@ -1,32 +1,19 @@
-#connection.py
 import mysql.connector
 import MySQLdb
+from dotenv import load_dotenv
+import os
 
-#LOCALHOST
-#def create_connection():
-#    connection = None
-#    try:
-#        connection = mysql.connector.connect(
-#            host="localhost",
-#            user="root",
-#            password="R@shini444",
-#            database="sql12707743"
-#        )
-#        if connection.is_connected():
-#            print("Connection to MySQL DB successful")
-#    except MySQLdb.Error as e:
-#        print(f"The error '{e}' occurred")
-#    return connection
+# Load environment variables from .env file
+load_dotenv()
 
-#ONLINE  HOST
 def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Rethinaath123",
-            database="sql12707743"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         if connection.is_connected():
             print("Connection to MySQL DB successful")
